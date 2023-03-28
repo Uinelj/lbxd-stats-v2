575582538ad846991f183cf4939ecd36351fd5c3
uj <aulien.jbadji@gmail.com>
2023-03-28 19:24:30 +0200

    try to add lunr

--- vite-project/src/lib/MoviePicker.svelte
+++ vite-project/src/lib/MoviePicker.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
     import { onMount } from "svelte";
+    //import { Index } from "lunr";
+    import lunr from "lunr";
+    // import lunr from "lunr";
 
     let movies = [];
+    let movies_for_index = [];
+    let idx;
     export let activeMovie;
 
     onMount(async () => {
@@ -15,7 +20,54 @@
         const res = await fetch(wl_url, options);
         movies = await res.json();
         movies.sort();
+
+        movies.forEach(function (movieid) {
+            movieid = movieid.split("-").join(" ");
+            movies_for_index.push({ name: movieid, text: movieid });
+        });
+
+        console.log(movies_for_index);
+        idx = lunr(function () {
+            this.ref("name");
+            this.field("text");
+
+            movies_for_index.forEach(function (doc) {
+                // doc = doc.split("-").join(" ");
+                this.add(doc);
+            }, this);
+        });
     });
+
+    //build index
+
+    // console.log(idx);
+    console.log(idx.search("avatar"));
+
+    // var documents = [
+    //     {
+    //         name: "Lunr",
+    //         text: "Lunr",
+    //     },
+    //     {
+    //         name: "React",
+    //         text: "React",
+    //     },
+    //     {
+    //         name: "Lodash",
+    //         text: "Lodash",
+    //     },
+    // ];
+
+    // var idx = lunr(function () {
+    //     this.ref("name");
+    //     this.field("text");
+
+    //     documents.forEach(function (doc) {
+    //         this.add(doc);
+    //     }, this);
+    // });
+
+    console.log(idx);
 </script>
 
 <div>
@@ -31,3 +83,8 @@
         {/each}
     </select>
 </div>
+<!-- <svelte:head>
+    <script type="module">
+        import "https://unpkg.com/lunr/lunr.js";
+    </script>
+</svelte:head> -->
